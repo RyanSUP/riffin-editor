e23825fa49fd5693deafd2fe0e5d5094365447df
RMo <morici23@gmail.com>
2022-04-20 19:51:37 -0400

    Handle duplicating a chord

--- tab-editor-2.js
+++ tab-editor-2.js
@@ -4,7 +4,7 @@ let dashGrid = document.querySelector('#dashGrid')
 // TODO: CALCULATE THIS
 const firstColumnIndexes = [0, 41, 82, 123, 164, 205]
 const lastColumnIndexes =  [40, 81, 122, 163, 204]
-const secondToLastCol = [38, 79, 120, 161, 202, 243]
+const secondToLastCol = [39, 80, 121, 162, 203]
 const LAST_INPUT_POSITION = 245
 
 const legalCharacters = {
@@ -15,7 +15,6 @@ const legalCharacters = {
   "x": handleAddCharacter, // mute
   "p": handleAddCharacter, // pull off
   "h": handleAddCharacter, // hammer on
-  "d": handleDuplicate, // duplicate
   "0": handleAddCharacter,
   "1": handleAddCharacter,
   "2": handleAddCharacter,
@@ -26,6 +25,7 @@ const legalCharacters = {
   "7": handleAddCharacter,
   "8": handleAddCharacter,
   "9": handleAddCharacter,
+  "d": handleDuplicate, // duplicate
   "Backspace" : handleRemoveCharacter,
 }
 
@@ -36,12 +36,8 @@ const arrows = {
   "ArrowUp" : true,
 }
 
-/* ----------- FUNCTIONS ------------ */
+/* ----------- UTILITY FUNCTIONS ------------ */
 
-// @param textArea
-// The textArea that will be filled
-// @param c
-// The value to  fill the area with.
 const fillTextArea = (textArea, c) => {
   for(let i = 0; i < 251; i++) {
     if(i%40 === 0 && i !== 0) textArea.value += '\n'
@@ -54,31 +50,80 @@ const updateCursorPosition = (newPosition) => {
   inputGrid.selectionEnd = newPosition
 }
 
-const updateGrid = (currentValue, cursorPosition, character) => {
+const getNewValues = (currentValue, cursorPosition, character) => {
   let currentValueAsArray =  [...currentValue]
   currentValueAsArray[cursorPosition] = character
   return currentValueAsArray.join('')
 }
 
+const updateGridValues = (cursorPosition, inputGridValue, dashGridValue) => {
+  inputGrid.value = getNewValues(inputGrid.value, cursorPosition, inputGridValue)
+  dashGrid.value = getNewValues(dashGrid.value, cursorPosition, dashGridValue)
+}
+
+
+const getPositionsAboveCursor = (cursorPosition) => {
+  let arrayOfPositions = []
+  for(let position = cursorPosition - 41; position >= 0; position -= 41) {
+    arrayOfPositions.push(position - 1)
+  }
+  return arrayOfPositions
+}
+
+const getPositionsBelowCursor = (cursorPosition) => {
+  let arrayOfPositions = []
+  for(let position = cursorPosition + 41; position <= 241; position += 41) {
+    arrayOfPositions.push(position - 1)
+  }
+  return arrayOfPositions
+}
+
+const getPositionsToDuplicate = (cursorPosition) => {
+  let aboveCursor = getPositionsAboveCursor(cursorPosition)
+  let belowCursor = getPositionsBelowCursor(cursorPosition)
+  let positions = [cursorPosition - 1, ...aboveCursor, ...belowCursor]
+  let gridValuesArray = [...inputGrid.value]
+  positions = positions.filter(position => gridValuesArray[position] !== ' ')
+  return positions
+}
+/* ----------- FUNCTIONS ------------ */
+const init = () => {
+  fillTextArea(inputGrid, ' ')
+  fillTextArea(dashGrid, '-')
+}
+
+/* ----------- EVENT HANDLERS ------------ */
 function handleAddCharacter(character) {
   let cursorPosition = inputGrid.selectionStart
   if(cursorPosition === LAST_INPUT_POSITION) return
   if(lastColumnIndexes.includes(cursorPosition)) return
-  inputGrid.value = updateGrid(inputGrid.value, cursorPosition, character)
-  dashGrid.value = updateGrid(dashGrid.value, cursorPosition, " ")
+  updateGridValues(cursorPosition, character, " ")
   updateCursorPosition(cursorPosition + 1)
 }
 
 function handleRemoveCharacter() {
   let cursorPosition = inputGrid.selectionStart - 1
   if(firstColumnIndexes.includes(cursorPosition)) return
-  inputGrid.value = updateGrid(inputGrid.value, cursorPosition, " ")
-  dashGrid.value = updateGrid(dashGrid.value, cursorPosition, "-")
+  updateGridValues(cursorPosition, " ", "-")
   updateCursorPosition(cursorPosition)
 }
 
 function handleDuplicate() {
-  console.log('duplicate')
+  cursorPosition = inputGrid.selectionStart
+  // Can't duplicate from these columns
+  if(firstColumnIndexes.includes(cursorPosition)) return
+  if(lastColumnIndexes.includes(cursorPosition)) return 
+  if(secondToLastCol.includes(cursorPosition)) return 
+
+  let positionsToDuplicate = getPositionsToDuplicate(cursorPosition)
+  if(positionsToDuplicate.length === 0) return
+
+  let gridValuesArray = [...inputGrid.value]
+  positionsToDuplicate.forEach(pos => {
+    updateGridValues(pos + 2, gridValuesArray[pos], " ")
+  })
+
+  updateCursorPosition(cursorPosition + 2)
 }
 
 /* ----------- LISTENERS ------------ */
@@ -94,7 +139,6 @@ inputGrid.addEventListener('paste', e => {
   e.preventDefault();
 });
 
-/* ----------- MAIN ------------ */
 
-fillTextArea(inputGrid, ' ')
-fillTextArea(dashGrid, '-')
+/* ----------- MAIN ------------ */
+init()
